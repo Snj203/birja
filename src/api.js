45726e99f37e603faeaa116b5cfa432e179d9384
Sanzhar <snjmbj099@gmail.com>
2025-05-25 17:18:05 +0600

    feat(refresh token): added api interceptor for 401 to use refresh

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import router from '@/router'
 
 const api = axios.create({
   baseURL: 'http://localhost:8081/api', // Адрес бэкенда
@@ -6,13 +7,42 @@ const api = axios.create({
 
 api.interceptors.request.use(config => {
   const user = localStorage.getItem('user');
-  if (user && user.token) {
-    config.headers.Authorization = `Bearer ${user.token}`;
+  if (user && user.access_token) {
+    config.headers.Authorization = `Bearer ${user.access_token}`;
   }
   return config;},
   error => {
     return Promise.reject(error);
   });
+
+api.interceptors.response.use(response => response, async error => {
+  const originalRequest = error.config;
+
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+
+    try {
+      const user = localStorage.getItem('user');
+      const response = await axios.post('http://localhost:8081/api/auth/refresh', null,
+        {
+          params:{
+            refreshToken:  user.refresh_token
+          }
+        });
+
+      localStorage.setItem('user', JSON.stringify(response.data));
+
+      originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+      return api(originalRequest);
+    } catch (refreshError) {
+      localStorage.removeItem('user');
+      await router.push('/login');
+      return Promise.reject(refreshError);
+    }
+  }
+
+  return Promise.reject(error);
+});
 
 export default {
   getAllAds() {
